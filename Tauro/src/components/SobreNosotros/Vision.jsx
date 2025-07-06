54122663e455deb5f953cd40c7463333998edc29
Mauro3jo <maurocrsiete@gmail.com>
2025-07-06 00:24:51 -0300

    componente visiion jsx y css

--- Tauro/src/components/SobreNosotros/Vision.jsx
+++ Tauro/src/components/SobreNosotros/Vision.jsx
@@ -1,7 +1,22 @@
+import './Vision.css';
+import toroVerde from '../../assets/ToroVerde.png';
+
 function Vision() {
   return (
-    <section className="vision">
-      {/* Contenido de Nuestra Visión */}
+    <section className="vision-section">
+      <div className="vision-card">
+        <img src={toroVerde} alt="Toro Tauro" className="toro-vision" draggable="false" />
+        <div className="vision-card-content">
+          <h3>NUESTRA VISIÓN</h3>
+          <p>
+            Tauro se visualiza como el <b>punto de encuentro</b> para una <b>comunidad de motociclistas</b> que valora la <b>seguridad y la calidad</b>.
+          </p>
+          <p>
+            Aspiramos a <b>transformar la compra</b> de neumáticos en una <span className="destacado">experiencia de valor agregado</span>, donde el conocimiento y el producto adecuado potencien cada una de sus aventuras.
+          </p>
+        </div>
+        <div className="degradado-verde"></div>
+      </div>
     </section>
   );
 }
