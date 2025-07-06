12f28b4c6d65e85b72a4ada8b70451b921d6d27f
Mauro3jo <maurocrsiete@gmail.com>
2025-07-05 22:11:11 -0300

    boton saber mas entre componentes

--- Tauro/src/pages/Home.jsx
+++ Tauro/src/pages/Home.jsx
@@ -1,14 +1,26 @@
+import './Home.css';
 import Hero from '../components/Home/Hero';
 import Beneficios from '../components/Home/Beneficios';
 import ProductosDestacados from '../components/Home/ProductosDestacados';
 import FormularioContacto from '../components/Home/FormularioContacto';
+import { useNavigate } from 'react-router-dom';
 
 function Home() {
+  const navigate = useNavigate();
+
   return (
     <>
       <Hero />
       <Beneficios />
-      <ProductosDestacados />
+      <div className="puente-boton-outer">
+        <ProductosDestacados />
+        <button
+          className="puente-boton"
+          onClick={() => navigate('/productos')}
+        >
+          Saber más
+        </button>
+      </div>
       <FormularioContacto />
     </>
   );
