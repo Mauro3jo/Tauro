6fb6cd5112a0a096dd2acb27712f451c97e0d4d3
Mauro3jo <maurocrsiete@gmail.com>
2025-07-06 00:13:18 -0300

    COMPONENTE MISION JSX Y CSS

--- Tauro/src/components/SobreNosotros/Mision.jsx
+++ Tauro/src/components/SobreNosotros/Mision.jsx
@@ -1,7 +1,24 @@
+import './Mision.css';
+import toroRojo from '../../assets/ToroRojo.png';
+
 function Mision() {
   return (
-    <section className="mision">
-      {/* Contenido de Nuestra Misión */}
+    <section className="mision-section">
+      <div className="mision-card">
+        <div className="mision-card-content">
+          <h3>NUESTRA MISIÓN</h3>
+          <p>
+            En Tauro, nuestra misión es <b>potenciar la experiencia</b> de cada motociclista
+            ofreciendo <span className="destacado">neumáticos de la más alta calidad.</span>
+          </p>
+          <p>
+            Nos comprometemos a brindar <b>seguridad superior</b> y un <b>rendimiento excepcional</b> en cada curva y recta,
+            asegurando que cada viaje sea tan emocionante como seguro.
+          </p>
+        </div>
+        <img src={toroRojo} alt="Toro Tauro" className="toro-mision" draggable="false" />
+        <div className="degradado-rojo"></div>
+      </div>
     </section>
   );
 }
